294c7469243f8ce4ec20c0418d6533c9f2983d88
Alvaro Perez <alvpdiaz@gmail.com>
2018-05-09 14:29:10 +0100

    Saving deployed contract address to a text file gp

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
+var fs = require('fs'); // Save deployed contract's address to text file
+
+
 var Voting = artifacts.require("Voting");
 
 module.exports = function(deployer) {
   deployer.deploy(Voting, ["edoardo","gopal","steve","tin","klaus","jie"]);
+  fs.writeFile("./smartContractAddress.txt",Voting.address,
+    function(err){if(err){console.log(err);} return;})
 };
