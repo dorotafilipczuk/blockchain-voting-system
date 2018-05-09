efb884e88f0ddccd9caea551dadedfd2f51fd5df
Alvaro Perez <alvpdiaz@gmail.com>
2018-05-09 15:41:44 +0100

    Minor changes

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -1,10 +1,14 @@
 var fs = require('fs'); // Save deployed contract's address to text file
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 var Voting = artifacts.require("Voting");
 
 module.exports = function(deployer) {
   deployer.deploy(Voting, ["edoardo","gopal","steve","tin","klaus","jie"]);
+  sleep(200);
   fs.writeFile("./smartContractAddress.txt",Voting.address,
-    function(err){if(err){console.log(err);} return;})
+               function(err){if(err){console.log(err);} return;})
 };
--- uglyFrontend/index.js
+++ uglyFrontend/index.js
@@ -1,7 +1,7 @@
 var web3 = new Web3(new Web3.providers.HttpProvider("https://3434ea7e.ngrok.io"));
 var abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
 var VotingContract = web3.eth.contract(abi);
-var contractInstance = VotingContract.at('0x371e179775186e6e98c6D3407446487E7a97B9d1');
+var contractInstance = VotingContract.at('0x371e179775186e6e98c6d3407446487e7a97b9d1');
 
 var candidates = {
   "edoardo" : "candidate-1", 
